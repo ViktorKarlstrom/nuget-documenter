91997b0ba285d10554f51dfe0cf906f212358ad6
Viktor Karlström <viktor.karlstrom@outlook.com>
2022-09-09 09:04:48 +0200

    Add PackageReference support

--- GenNugetDoc.ps1
+++ GenNugetDoc.ps1
@@ -1,12 +1,19 @@
 function GenNugetDoc($BaseDirectory) {
-
-
-    $userInput = Read-Host -Prompt "Press Enter for latest stable, enter '9' for prerelease"
-    $getPrerelease = "false"
+    $packageReferenceInput = Read-Host -Prompt "1 for PackageReference`n2 for packages.config`n"
+    Write-Host ''
+    $versionInput = Read-Host -Prompt "1 for latest stable`n2 for prerelease`n"
     
-    if($userInput -eq '9'){
+    $isPackageReference = "false"
+    $versionInput = "false"
+
+    if($packageReferenceInput -eq '1') {
+        $isPackageReference = "true"
+    }
+    
+    if($versionInput -eq '2'){
         $getPrerelease = "true"
     }
+    Write-Host ''
     Write-Host "This might take a minute, go get a coffee..."
 
     # Create an Excel object
@@ -34,7 +41,7 @@ function GenNugetDoc($BaseDirectory) {
     $ExcelWorkSheet.Columns.Item(2).NumberFormat = "@"
     $ExcelWorkSheet.Columns.Item(4).NumberFormat = "@"
 
-    # Make the table head bold, set the font size and turn on filtering
+    # Make the table head bold, set the font size and the column width
     $ExcelWorkSheet.Rows.Item(1).Font.Bold = $true
     $ExcelWorkSheet.Rows.Item(1).Font.size = 12
     $ExcelWorkSheet.Rows.Item(1).AutoFilter() | Out-Null
@@ -42,38 +49,75 @@ function GenNugetDoc($BaseDirectory) {
     $ExcelWorkSheet.Columns.Format
 
     # Recursively get all the packages.config. Exclude config in folder "PackageTmp".
-    $PACKAGECONFIGS = Get-ChildItem -Recurse -Force $BaseDirectory -ErrorAction SilentlyContinue |
+    if ($isPackageReference -eq "true") {
+        $packageFiles = Get-ChildItem -Recurse -Force $BaseDirectory -ErrorAction SilentlyContinue |
+        Where-Object { (( $_.Name -Like "*.csproj")) -and $(Split-Path (Split-Path $_.FullName -Parent) -Leaf) -notlike 'PackageTmp'} 
+    } else {
+        $packageFiles = Get-ChildItem -Recurse -Force $BaseDirectory -ErrorAction SilentlyContinue |
         Where-Object { (( $_.Name -eq "packages.config")) -and $(Split-Path (Split-Path $_.FullName -Parent) -Leaf) -notlike 'PackageTmp'} 
-
+    }
     $counter = 2
 
-    ForEach($PACKAGECONFIG in $PACKAGECONFIGS)
+    ForEach($packageFile in $packageFiles)
     {
-
-        $path = $PACKAGECONFIG.FullName
+        $path = $packageFile.FullName
         [xml]$packages = Get-Content $path
 
-        ForEach($package in $packages.packages.package)
+        if($isPackageReference -eq "true")
         {
-            $response = Invoke-RestMethod "https://azuresearch-usnc.nuget.org/query?q=$($package.id)&prerelease=$($getPrerelease)" -contenttype 'application/json'
-            $project = Split-Path (Split-Path $path -Parent) -Leaf
+            ForEach($package in $packages.Project.ItemGroup.PackageReference) 
+            {
+                if ([string]::IsNullOrEmpty($package.Include)) 
+                {
+                    continue
+                }
 
-            $ExcelWorkSheet.Columns.Item(1).Rows.Item($counter) = $package.id
-            $ExcelWorkSheet.Columns.Item(2).Rows.Item($counter) = $package.version
-            $ExcelWorkSheet.Columns.Item(4).Rows.Item($counter) = $package.version
-            
-            if($response.totalHits -gt 0) {
-                $ExcelWorkSheet.Columns.Item(3).Rows.Item($counter) = $response.data[0].version
-                $ExcelWorkSheet.Cells.Item($counter,5).FormulaLocal = "=(IF(VALUE(LEFT(C$counter;(FIND(`".`";C$counter;1)-1))) > VALUE(LEFT(B$counter;(FIND(`".`";B$counter;1)-1))); `"Yes`"; `"No`"))"
-                $ExcelWorkSheet.Cells.Item($counter,6).FormulaLocal = "=IF(G$counter=`"Uninstalled`";`"N/A`";IF(C$counter=D$counter; `"Yes`"; `"No`"))"
+                $response = Invoke-RestMethod "https://azuresearch-usnc.nuget.org/query?q=$($package.Include)&prerelease=$($getPrerelease)" -contenttype 'application/json'
+                $project = Split-Path (Split-Path $path -Parent) -Leaf
 
-            } else {
-                $ExcelWorkSheet.Columns.Item(3).Rows.Item($counter) = "Not found"
+                $ExcelWorkSheet.Columns.Item(1).Rows.Item($counter) = $package.Include
+                $ExcelWorkSheet.Columns.Item(2).Rows.Item($counter) = $package.Version
+                $ExcelWorkSheet.Columns.Item(4).Rows.Item($counter) = $package.Version 
+                
+                if($response.totalHits -gt 0) {
+                    $ExcelWorkSheet.Columns.Item(3).Rows.Item($counter) = $response.data[0].version
+                    $ExcelWorkSheet.Cells.Item($counter,5).FormulaLocal = "=(IF(VALUE(LEFT(C$counter;(FIND(`".`";C$counter;1)-1))) > VALUE(LEFT(B$counter;(FIND(`".`";B$counter;1)-1))); `"Yes`"; `"No`"))"
+                    $ExcelWorkSheet.Cells.Item($counter,6).FormulaLocal = "=IF(G$counter=`"Uninstalled`";`"N/A`";IF(C$counter=D$counter; `"Yes`"; `"No`"))"
+
+                } else {
+                    $ExcelWorkSheet.Columns.Item(3).Rows.Item($counter) = "Not found"
+                }
+
+                $ExcelWorkSheet.Columns.Item(8).Rows.Item($counter) = $project
+
+                $counter++
             }
+        } 
+        else 
+        {
+            ForEach($package in $packages.packages.package)
+            {
+                $response = Invoke-RestMethod "https://azuresearch-usnc.nuget.org/query?q=$($package.id)&prerelease=$($getPrerelease)" -contenttype 'application/json'
+                $project = Split-Path (Split-Path $path -Parent) -Leaf
 
-            $ExcelWorkSheet.Columns.Item(8).Rows.Item($counter) = $project
 
-            $counter++
+                $ExcelWorkSheet.Columns.Item(1).Rows.Item($counter) = $package.id
+                $ExcelWorkSheet.Columns.Item(2).Rows.Item($counter) = $package.version
+                $ExcelWorkSheet.Columns.Item(4).Rows.Item($counter) = $package.version
+                
+                if($response.totalHits -gt 0) {
+                    $ExcelWorkSheet.Columns.Item(3).Rows.Item($counter) = $response.data[0].version
+                    $ExcelWorkSheet.Cells.Item($counter,5).FormulaLocal = "=(IF(VALUE(LEFT(C$counter;(FIND(`".`";C$counter;1)-1))) > VALUE(LEFT(B$counter;(FIND(`".`";B$counter;1)-1))); `"Yes`"; `"No`"))"
+                    $ExcelWorkSheet.Cells.Item($counter,6).FormulaLocal = "=IF(G$counter=`"Uninstalled`";`"N/A`";IF(C$counter=D$counter; `"Yes`"; `"No`"))"
+
+                } else {
+                    $ExcelWorkSheet.Columns.Item(3).Rows.Item($counter) = "Not found"
+                }
+
+                $ExcelWorkSheet.Columns.Item(8).Rows.Item($counter) = $project
+
+                $counter++
+            }
         }
     }
 
@@ -81,7 +125,7 @@ function GenNugetDoc($BaseDirectory) {
     $usedRange = $ExcelWorkSheet.UsedRange	
     $usedRange.EntireColumn.AutoFit() | Out-Null
     
-    # Save the report and close Excel:
+    # Save the report:
     $timestamp = (Get-Date -UFormat "%Y-%m-%d_%H-%M-%S").tostring()
     $fileName = "NuGet-$($timestamp).xlsx";
     $location = "C:"
